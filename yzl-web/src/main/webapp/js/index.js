923e0d5e7a7a9093eae13e69c252c20e21b50615
gunners_k <gunners_k@aliyun.com>
2019-07-14 13:01:08 +0800

    com dev2

--- yzl-web/src/main/webapp/js/index.js
+++ yzl-web/src/main/webapp/js/index.js
@@ -4,7 +4,7 @@ $(function(){
 						.write("<script src='assets/js/jquery-2.0.3.min.js'>"
 								+ "<"+"script>");
 		
-//father you am father dev1 dev11 
+//father you am father dev1 dev22 
 
 		if ("ontouchend" in document)
 			document
