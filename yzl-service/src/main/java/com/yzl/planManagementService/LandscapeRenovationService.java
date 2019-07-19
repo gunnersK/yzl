eaf720ff37a85550e6de2bd5648d202fa892d9a6
gunners_k <gunners_k@aliyun.com>
2019-07-19 10:02:42 +0800

    fix gitignore again

--- yzl-service/src/main/java/com/yzl/planManagementService/LandscapeRenovationService.java
+++
@@ -1,9 +0,0 @@
-package com.yzl.planManagementService;
-
-import com.yzl.utils.EasyUIResult;
-
-public interface LandscapeRenovationService {
-
-	EasyUIResult findAll(int page, int rows);
-
-}
--- yzl-service/src/main/java/com/yzl/planManagementService/QualityPromotionService.java
+++
@@ -1,9 +0,0 @@
-package com.yzl.planManagementService;
-
-import com.yzl.utils.EasyUIResult;
-
-public interface QualityPromotionService {
-
-	EasyUIResult findAll(int page, int rows);
-
-}
